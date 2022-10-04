ab89b8a0363365f5bf94a880b7a7c9b6e51a547a
Philip Hagemann <philip.hagemann@tum.de>
2022-10-04 19:32:13 +0200

    fixed migration stuff

--- backend/basic/management/commands/add_users.py
+++ backend/basic/management/commands/add_users.py
@@ -8,16 +8,10 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         UserModel = get_user_model()
 
-        if not UserModel.objects.filter(username='robert_admin').exists():
-            user_1 = UserModel.objects.create_user('robert_admin', password='robert1234')
+        if not UserModel.objects.filter(username='admin').exists():
+            user_1 = UserModel.objects.create_user('admin', password='admin1234')
             user_1.is_superuser = True
             user_1.is_staff = True
             user_1.save()
-
-        if not UserModel.objects.filter(username='hagi_admin').exists():
-            user_2 = UserModel.objects.create_user('hagi_admin', password='hagi1234')
-            user_2.is_superuser = True
-            user_2.is_staff = True
-            user_2.save()
 
         print('user created')
