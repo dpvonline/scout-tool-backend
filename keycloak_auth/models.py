097ede139aae2150c400ab0fe617d7073309ff7e
Philip <philiphagemann@icloud.com>
2022-10-24 12:07:47 +0200

    *Added Models for cloning Keycloak Groups and Roles *Added script for fetching keycloak groups *adjusted other models for using the new keycloak models

---
+++ keycloak_auth/models.py
@@ -0,0 +1,23 @@
+import uuid
+
+from django.db import models
+
+
+class KeycloakRole(models.Model):
+    id = models.UUIDField(auto_created=True, primary_key=True, default=uuid.uuid4, editable=False)
+    keycloak_id = models.CharField(max_length=36)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class KeycloakGroup(models.Model):
+    id = models.UUIDField(auto_created=True, primary_key=True, default=uuid.uuid4, editable=False)
+    keycloak_id = models.CharField(max_length=36, unique=True)
+    name = models.CharField(max_length=100)
+    roles = models.ManyToManyField(KeycloakRole, blank=True)
+    parent = models.ForeignKey('self', null=True, on_delete=models.PROTECT, related_name='keycloak_group', blank=True)
+
+    def __str__(self):
+        return self.name
