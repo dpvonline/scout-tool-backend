25dcd575eb11fce06871ed937a5b065397c4b73d
Philip <philiphagemann@icloud.com>
2023-01-06 21:48:37 +0100

    *Added redirect of plain django site to front url

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import index
 
 urlpatterns = [
+    path('', index.RedirectView.as_view()),
     path('admin/', admin.site.urls),
     path('basic/', include('basic.urls')),
     path('auth/', include('authentication.urls')),
@@ -10,4 +12,3 @@ urlpatterns = [
     path('food/', include('food.urls')),
     path('keycloak/', include('keycloak_auth.urls')),
 ]
-
