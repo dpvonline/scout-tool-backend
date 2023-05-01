4e75a36aa720d15eae3db24341623133c3047115
Philip <philiphagemann@icloud.com>
2023-05-01 18:52:23 +0200

    Added new attribute model Added migrations

--- anmelde_tool/attributes/admin.py
+++ anmelde_tool/attributes/admin.py
@@ -1,55 +1,40 @@
-# from django.contrib import admin
-# from polymorphic.admin import PolymorphicChildModelAdmin, PolymorphicParentModelAdmin, PolymorphicChildModelFilter
-#
-# from .models import AbstractAttribute, BooleanAttribute, TimeAttribute, IntegerAttribute, FloatAttribute, \
-#     TravelAttribute, StringAttribute
-#
-#
-# class AbstractAttributeChildAdmin(PolymorphicChildModelAdmin):
-#     """ Base admin class for all child models """
-#     base_model = AbstractAttribute  # Optional, explicitly set here.
-#     list_display = ('name', 'type', 'template')
-#     search_fields = ('name', 'type')
-#     autocomplete_fields = ('type',)
-#     show_in_index = True
-#
-#
-# @admin.register(BooleanAttribute)
-# class BooleanAttributeAdmin(AbstractAttributeChildAdmin):
-#     base_model = BooleanAttribute
-#
-#
-# @admin.register(TimeAttribute)
-# class TimeAttributeAdmin(AbstractAttributeChildAdmin):
-#     base_model = TimeAttribute
-#
-#
-# @admin.register(IntegerAttribute)
-# class IntegerAttributeAdmin(AbstractAttributeChildAdmin):
-#     base_model = IntegerAttribute
-#
-#
-# @admin.register(FloatAttribute)
-# class FloatAttributeAdmin(AbstractAttributeChildAdmin):
-#     base_model = FloatAttribute
-#
-#
-# @admin.register(TravelAttribute)
-# class TravelAttributeAdmin(AbstractAttributeChildAdmin):
-#     base_model = TravelAttribute
-#
-#
-# @admin.register(StringAttribute)
-# class StringAttributeAdmin(AbstractAttributeChildAdmin):
-#     base_model = StringAttribute
-#
-#
-# @admin.register(AbstractAttribute)
-# class AbstractAttributeParentAdmin(PolymorphicParentModelAdmin):
-#     """ The parent model admin """
-#     base_model = AbstractAttribute  # Optional, explicitly set here.
-#     child_models = (
-#         BooleanAttribute, TimeAttribute, IntegerAttribute, FloatAttribute, TravelAttribute,
-#         StringAttribute)
-#     list_filter = (PolymorphicChildModelFilter,)  # This is optional.
-#     list_display = ('name', 'type', 'template')
+from django.contrib import admin
+
+from .models import BooleanAttribute, TimeAttribute, IntegerAttribute, FloatAttribute, \
+    TravelAttribute, StringAttribute
+
+
+class BaseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'registration')
+    search_fields = ('name',)
+
+
+@admin.register(BooleanAttribute)
+class BooleanAttributeAdmin(BaseAdmin):
+    pass
+
+
+@admin.register(TimeAttribute)
+class TimeAttributeAdmin(BaseAdmin):
+    pass
+
+
+@admin.register(IntegerAttribute)
+class IntegerAttributeAdmin(BaseAdmin):
+    pass
+
+
+@admin.register(FloatAttribute)
+class FloatAttributeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'registration')
+    search_fields = ('name',)
+
+
+@admin.register(TravelAttribute)
+class TravelAttributeAdmin(BaseAdmin):
+    pass
+
+
+@admin.register(StringAttribute)
+class StringAttributeAdmin(BaseAdmin):
+    pass
