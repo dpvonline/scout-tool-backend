fc8862d50c344c77ecf3931955001b42be534083
FinnMeMo <finn.moltmann@gmail.com>
2023-10-25 21:20:49 +0200

    Add method to check for double participants

--- anmelde_tool/registration/api_exceptions.py
+++ anmelde_tool/registration/api_exceptions.py
@@ -7,6 +7,12 @@ class ZipCodeNotFound(APIException):
     default_code = 'not_found'
 
 
+class ParticipantAlreadyExists(APIException):
+    status_code = 400
+    default_detail = 'Teilnehmer ist bereits angemeldet.'
+    default_code = 'bad request'
+
+
 class PersonAlreadyRegistered(APIException):
     status_code = 404
     default_detail = 'Die angegebene Person wurde bereits angemeldet.'
