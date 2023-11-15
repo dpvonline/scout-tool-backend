98e498bb5a20583a74ce483035fc7b2760d1dad1
Philip <philiphagemann@icloud.com>
2023-11-15 19:26:52 +0100

    Improve raw zipcode to database zipcode

--- anmelde_tool/registration/api_exceptions.py
+++ anmelde_tool/registration/api_exceptions.py
@@ -1,12 +1,6 @@
 from rest_framework.exceptions import APIException
 
 
-class ZipCodeNotFound(APIException):
-    status_code = 404
-    default_detail = 'Die angegebene PLZ wurde nicht gefunden.'
-    default_code = 'not_found'
-
-
 class ParticipantAlreadyExists(APIException):
     status_code = 400
     default_detail = 'Teilnehmer ist bereits angemeldet.'
